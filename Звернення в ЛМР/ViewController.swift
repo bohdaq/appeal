35c41e1ebaeb38dbda6c1dc98839e24ddc4b89ae
Bogdan Tsap <bohdaq@gmail.com>
2016-02-05 10:53:48 +0200

    Initial Commit

---
+++ Звернення в ЛМР/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Звернення в ЛМР
+//
+//  Created by Bogdan on 2/5/16.
+//  Copyright © 2016 Bogdan Tsap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
